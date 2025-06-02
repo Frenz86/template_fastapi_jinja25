d2268c600581a2e0266ec1894149a7b48d1837ad
Frenz86 <frenzy86@gmail.com>
2025-06-02 16:02:43 +0200

    new

--- app/database.py
+++ app/database.py
@@ -4,18 +4,18 @@ from sqlalchemy.orm import sessionmaker
 import os
 from dotenv import load_dotenv
 
-# Database URL from environment
+# Database URL from environment # decommentare
 
 # Carica le variabili d'ambiente dal file .env
-load_dotenv()
+# load_dotenv()
 
-POSTGRES_USER = os.getenv('POSTGRES_USER')
-POSTGRES_PASSWORD = os.getenv('POSTGRES_PASSWORD')
-POSTGRES_DB = os.getenv('POSTGRES_DB')
-POSTGRES_HOST = os.getenv('POSTGRES_HOST')
-POSTGRES_PORT = os.getenv('POSTGRES_PORT')
+# POSTGRES_USER = os.getenv('POSTGRES_USER')
+# POSTGRES_PASSWORD = os.getenv('POSTGRES_PASSWORD')
+# POSTGRES_DB = os.getenv('POSTGRES_DB')
+# POSTGRES_HOST = os.getenv('POSTGRES_HOST')
+# POSTGRES_PORT = os.getenv('POSTGRES_PORT')
 
-DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
+# DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
 
 
 # Database URL from environment - SQLite configuration

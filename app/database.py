9c8cdd2ecdced397ea23dd0347f4604f2ed318a2
Frenz86 <frenzy86@gmail.com>
2025-06-02 16:01:01 +0200

    new

--- app/database.py
+++ app/database.py
@@ -2,9 +2,21 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
+from dotenv import load_dotenv
 
 # Database URL from environment
-#DATABASE_URL = os.getenv('DATABASE_URL', default='postgresql://postgres:password123@localhost:5432/fastapi_db')
+
+# Carica le variabili d'ambiente dal file .env
+load_dotenv()
+
+POSTGRES_USER = os.getenv('POSTGRES_USER')
+POSTGRES_PASSWORD = os.getenv('POSTGRES_PASSWORD')
+POSTGRES_DB = os.getenv('POSTGRES_DB')
+POSTGRES_HOST = os.getenv('POSTGRES_HOST')
+POSTGRES_PORT = os.getenv('POSTGRES_PORT')
+
+DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
+
 
 # Database URL from environment - SQLite configuration
 DATABASE_URL = os.getenv('DATABASE_URL', default='sqlite:///./fastapi_app.db')
